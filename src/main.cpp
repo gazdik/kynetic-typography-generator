3aa43a1d16f5c43d20f0aafabf63f73919c8d84e
Peter Gazdik <xgazdi03@stud.fit.vutbr.cz>
2016-11-22 14:19:01 +0100

    Simple demonstration of actions

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,19 @@
 #include <GLFW/glfw3.h>
 #include <FTGL/ftgl.h>
 
-#define glm_force_radians
+#define GLM_FORCE_RADIANS
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
 #include <GLProgram.h>
 #include <GLView.h>
+#include "triangle.h"
+#include "actionmanager.h"
+#include "action.h"
 
 #include <cstdio>
+#include <iostream>
 using namespace std;
 
 static GLint vertexCoordAttribute;
@@ -29,6 +33,7 @@ const float OY = 40;
 
 static GLView *glView;
 static GLProgram *program;
+static Triangle *triangle1, *triangle2;
 
 // FTGL vars
 static FTFont *font;
@@ -36,7 +41,7 @@ FTSimpleLayout simpleLayout;
 
 static const char vs_source[] =
 {
-"#version 150                                                   \n"
+"#version 330                                                   \n"
 "                                                               \n"
 "uniform mat4 mvp;                                              \n"
 "uniform vec3 pen;                                              \n"
@@ -50,13 +55,13 @@ static const char vs_source[] =
 // "  f_color = vec3(-v_coord.z/100,0.4,0.9);                    \n"
 //"  f_color = vec3((v_normal.x+0.5)/2,(v_normal.y+0.5)/2,(v_normal.z+0.5)/2);  \n"
 //"  f_color = vec3((v_normal.x)*0.1,(v_normal.y)*0.1,(v_normal.z)*0.1);  \n"
-"  f_color = vec3(0.0f, 0.0f, 0.0f);  \n"
+"  f_color = vec3(0.0f, 0.0f, 0.0f) + v_normal * 0.1;  \n"
 "}                                                              \n"
 };
 
 static const char fs_source[] =
 {
-"#version 150                                                   \n"
+"#version 330                                                   \n"
 "                                                               \n"
 "in      vec3 f_color;                                          \n"
 "out     vec4 fragColor;                                        \n"
@@ -73,26 +78,18 @@ static void RenderScene(void)
 
     glClearColor(1,1,1,1);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
     glEnable(GL_DEPTH_TEST);
 
-    float angle = now * 45;
-    glm::vec3 axis_y(0, 1, 0);
-    glm::mat4 anim = glm::rotate(glm::mat4(1.0f), glm::radians(angle), axis_y);
+    ActionManager::getInstance()->update(now);
 
-    glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.01, 0.01, 0.01));
-    glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(-0.2, 0.3, -4.0));
+    glm::mat4 projection = glm::ortho(0.0f, (float) glView->getWidth(), 0.0f, (float) glView->getHeight(), -10.0f, 100.0f);
 
-    glm::mat4 view = glm::lookAt(glm::vec3(0.0, 2.0, 0.0), glm::vec3(0.0, 0.0, -4.0), glm::vec3(0.0, 1.0, 0.0));
-    glm::mat4 projection = glm::perspective(45.0f, 1.0f*glView->getWidth() / glView->getHeight(), 1.0f, 20.0f);
-
-    glm::mat4 mvp = projection * view * model * scale * anim;
-
+    // Render font (transformation doesn't work!)
     program->use();
-    program->setUniformMatrix4fv(mvpUniform, glm::value_ptr(mvp), 1);
-
-//    simpleLayout.Render(myString, -1, FTPoint(), FTGL::RENDER_SIDE);
+    program->setUniformMatrix4fv(mvpUniform, glm::value_ptr(projection), 1);
     simpleLayout.Render(myString, -1, FTPoint(), FTGL::RENDER_FRONT);
+
+    triangle1->render(projection);
 
     glView->swapBuffers();
 
@@ -111,7 +108,7 @@ int main(int argc, char **argv)
 {
     char const *file = "fonts/DejaVuSerif-Bold.ttf";
 
-    glView = new GLView("KyneTypoGen", 640, 480);
+    glView = new GLView("KyneTypoGen", 800, 600);
     GLFWwindow *window = glView->getWindow();
 
     program = new GLProgram(vs_source, fs_source);
@@ -119,6 +116,27 @@ int main(int argc, char **argv)
     vertexNormalAttribute = program->getAttribLocation("v_normal");
     mvpUniform = program->getUniformLocation("mvp");
     vertexOffsetUniform = program->getUniformLocation("pen");
+
+    triangle1 = new Triangle();
+    triangle1->setContentSize(100.0f, 100.0f);
+    triangle1->setPosition(100, 100);
+    triangle1->setRotation(-45.0f);
+    triangle1->setColor(0.99, 0.26, 0.6);
+
+    triangle2 = new Triangle();
+    triangle2->setContentSize(50.0f, 50.0f);
+    triangle2->setPosition(0.0f, 75.0f);
+    triangle2->setColor(0.99, 0.7, 0.1);
+    triangle2->setAlpha(0.0f);
+    triangle2->runAction(new FadeIn(2.0f));
+    triangle2->runAction(new RotateTo(2.0f, 360.0f));
+    triangle2->runAction(new ScaleTo(2.0f, 3.0f, 3.0f));
+
+    triangle1->addChild(triangle2);
+    triangle1->setAlpha(1.0f);
+    triangle1->runAction(new FadeOut(2.0f));
+    triangle1->runAction(new MoveTo(2.0f, glm::vec2(400.0f, 400.0f)));
+    triangle1->runAction(new RotateTo(2.0f, 360.0f));
 
     glEnable(GL_BLEND);
     glEnable(GL_DEPTH_TEST);
@@ -137,7 +155,6 @@ int main(int argc, char **argv)
     font->ShaderLocations(vertexCoordAttribute, vertexNormalAttribute, vertexOffsetUniform);
     font->FaceSize(90);
     font->Depth(3);
-//    font->Outset(1, 2);
     font->CharMap(ft_encoding_unicode);
 
     simpleLayout.SetFont(font);
