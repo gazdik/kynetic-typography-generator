6128f0777aa8800f45ce6a9efde5b71a263b3527
Jan Hrdina <jan.hrdka@gmail.com>
2016-11-29 16:04:30 +0100

    Effect: OneWord improved to use fixed Fade-in

--- src/effects/oneword.cpp
+++ src/effects/oneword.cpp
@@ -29,20 +29,20 @@ float OneWord::run(InputString &inputString, Node &inputNode, float startTime)
 
     for (int i = 0; i < w.size(); ++i) {
         auto sw = new Text(w.substr(i, 1), "Roboto-Thin");
-        sw->setAlpha(.0f);
+        sw->setAlpha(0);
         sw->setPosition(owLeft + i * 120, bottom);
         sw->setScale(2);
 
         // Fade in on start
-//        sw->runAction(startTime + i * 0.3f,
-//                      new FadeIn(0.3));
+        sw->runAction(startTime + i * 0.1f,
+                      new FadeIn(0.3));
         // Subtle zoom-in of each letter during the whole animation
 
-        sw->runAction(startTime + i * 0.3f,
+        sw->runAction(startTime + i * 0.2f,
                       //new EaseIn(new MoveBy(4, glm::vec3(0, 0, 20))));
                       new EaseOut(new ScaleBy(4, .2f)));
         sw->runAction(startTime + 0.3f + i * 0.3f,
-                      new FadeOut(0.6));
+                      new FadeOut(1));
         inputNode.addChild(sw);
     }
 
@@ -53,9 +53,9 @@ float OneWord::run(InputString &inputString, Node &inputNode, float startTime)
         new MoveTo(0, glm::vec3(0, -1080, 0))
     );
 
-//    inputNode.runAction(startTime + dur,
-//        new FadeOut(0.5)
-//    );
+    inputNode.runAction(startTime + dur - 0.3f,
+        new FadeOut(0.3)
+    );
 
     return dur;
 }
