a9d915093a817b042a986c5a7a6c0aced33b5e1a
Jan Hrdina <jan.hrdka@gmail.com>
2016-12-04 14:08:07 +0100

    Download fonts script

---
+++ fonts/downloadFonts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FIT_PREFIX='http://www.stud.fit.vutbr.cz/~xhrdin10/pgr/'
+wget "${FIT_PREFIX}DejaVuSerif-Bold.ttf"
+wget "${FIT_PREFIX}FiraMono-Bold.ttf"
+wget "${FIT_PREFIX}FiraMono-Medium.ttf"
+wget "${FIT_PREFIX}FiraMono-Regular.ttf"
+wget "${FIT_PREFIX}Roboto-Bold.ttf"
+wget "${FIT_PREFIX}Roboto-Light.ttf"
+wget "${FIT_PREFIX}Roboto-Medium.ttf"
+wget "${FIT_PREFIX}Roboto-Regular.ttf"
+wget "${FIT_PREFIX}Roboto-Thin.ttf"
